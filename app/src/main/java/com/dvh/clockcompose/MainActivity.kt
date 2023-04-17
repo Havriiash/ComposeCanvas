2daf91d402ff2cb8c984666a41feb6120af322cc
dmytroh <dv.gromov1207@gmail.com>
2023-04-17 19:26:16 +0300

    remove unused imports

--- app/src/main/java/com/dvh/clockcompose/MainActivity.kt
+++ app/src/main/java/com/dvh/clockcompose/MainActivity.kt
@@ -5,11 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.dvh.clockcompose.ui.theme.ClockComposeTheme
 
 class MainActivity : ComponentActivity() {
